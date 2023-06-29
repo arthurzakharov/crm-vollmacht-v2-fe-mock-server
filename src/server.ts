7f705bb24f42de714a930d844a5e408cb24f4f81
arthur.zakharov <arthur.zakharov@legal.one>
2023-06-29 11:41:50 +0200

    CORS allow *

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { delay } from "./middlewares";
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173']
+    origin: "*"
 }));
 app.use(json());
 app.use(morgan("tiny"));
